61e42ae82dc1318b7270e33aab7372cef461097d
Fernando Carminatti <nandocarminatti@hotmail.com>
2024-09-03 17:25:28 -0300

    Fix: Removed Deprecated Marked values from the response entity return.

--- src/main/java/com/example/Motto/MD/Controller/BikeTransportationVehicleController.java
+++ src/main/java/com/example/Motto/MD/Controller/BikeTransportationVehicleController.java
@@ -4,10 +4,10 @@ import com.example.Motto.MD.Dto.BikeTransportationVehicleDto;
 import com.example.Motto.MD.Service.BikeTransportationVehicleService;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,9 +27,9 @@ public class BikeTransportationVehicleController {
         try {
             bikeTransportationVehicleService.createBikeTransportationVehicle(bikeTransportationVehicleDto);
         } catch (DataIntegrityViolationException dataIntegrityViolationException) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("{ \n Error: Duplicate \n}");
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body("{ \n Error: Duplicate \n}");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{ \n Error: Internal Server Error \n}");
+            return ResponseEntity.status(HttpStatusCode.valueOf(500)).body("{ \n Error: Internal Server Error \n}");
         }
         return ResponseEntity.ok().build();
     }
