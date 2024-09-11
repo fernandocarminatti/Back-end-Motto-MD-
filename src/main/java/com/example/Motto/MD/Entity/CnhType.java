269ca7c7d918c1b41849c7be4a70b68c27cbaa22
Fernando Carminatti <nandocarminatti@hotmail.com>
2024-09-11 13:32:54 -0300

    Fix: Clear bad imports and SignUp Validation to a String regex instead of a complex Enum Validator

--- src/main/java/com/example/Motto/MD/Entity/CnhType.java
+++ src/main/java/com/example/Motto/MD/Entity/CnhType.java
@@ -1,33 +1,7 @@
 package com.example.Motto.MD.Entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
-import java.util.Arrays;
-
 public enum CnhType {
-    A("A"),
-    B("B"),
-    AB("AB");
-
-    private final String value;
-
-    CnhType(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return this.name();
-    }
-
-    public static CnhType fromValue(String value) {
-        for (CnhType type : values()) {
-            if (type.value.equals(value)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid CnhType value: " + value + ". Valid values are: " + Arrays.toString(values()));
-    }
+    A(),
+    B(),
+    AB();
 }
